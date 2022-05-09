f8e27a4e6f1abd8b9451bd0dcd089de533d0620f
Ricky <shit6333@gmail.com>
2022-05-09 16:16:07 +0800

    Add Controllable Platform

---
+++ Practice_game_2/Assets/Movement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 1.0f;
+    // public float jumpForce = 10f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D)) rb.velocity = new Vector2(speed * Time.deltaTime, rb.velocity.y);
+        if (Input.GetKey(KeyCode.A)) rb.velocity = new Vector2(-speed * Time.deltaTime, rb.velocity.y);
+        // if (Input.GetKey(KeyCode.Space)) rb.AddForce( new Vector2(0f,jumpForce) );
+    }
+}
